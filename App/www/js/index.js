4841ac30eb18489bd16c1adbe51e995d10ad6286
Cameron Lilly <cameronlilly@maurasoftware.com>
2015-06-09 15:57:26 -0600

    changed json structure, added more content

--- App/www/js/index.js
+++ App/www/js/index.js
@@ -38,15 +38,26 @@ inspection.config(['$routeProvider',
             })
 
             .otherwise({
-                redirectTo: "/create"
+                redirectTo: '/create'
             });
   }]);
 
 // Config - the material design theme
 inspection.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
-    .primaryPalette('green')
-    .accentPalette('blue');
+    .primaryPalette('green', {
+      'default': '500', // by default use shade 400 from the pink palette for primary intentions
+      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+    })
+    .accentPalette('orange')
+//      , {
+//      'default': '500', // by default use shade 400 from the pink palette for primary intentions
+//      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+//      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+//      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+//    });
 });
 
 // Controller for the index page
@@ -155,4 +166,3 @@ inspection.factory('inspectionService', ['$http', '$cacheFactory', '$route',
   }]);
 
 
- 

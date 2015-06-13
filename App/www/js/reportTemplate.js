2bb47ca4be6cb8c8a07d0d5110f294137314c581
Stephen Beacham <sbeacham@maurasoftware.com>
2015-06-13 10:57:21 -0600

    I added in comment that only helps me.

--- App/www/js/reportTemplate.js
+++ App/www/js/reportTemplate.js
@@ -1471,7 +1471,7 @@ $scope.items = [
                              }
                      ]
 
-                 },
+                 }, //This is "Checkboxes" Close Curly-Bracket.
 
                      ]
 

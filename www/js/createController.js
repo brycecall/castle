796ba6a6584ceb76805aca05f963687b7c18a7b4
Cameron Lilly <cameronlilly@maurasoftware.com>
2015-07-11 15:33:11 -0600

    Fixed md-header on large size width,  Added Camera functionality

--- www/js/createController.js
+++ www/js/createController.js
@@ -69,4 +69,58 @@
          return obj ? Object.keys(obj) : [];
      };
 
+//
+//     $scope.$watch(function() { return $mdMedia('gt-md'); }, function(big) {
+//        console.log(big);
+//      });
+
+     $scope.images = [];
+
+ function onPhotoURISuccess(imageURI) {
+      // Uncomment to view the image file URI
+      // console.log(imageURI);
+
+      // Get image handle
+      //
+//      var largeImage = document.getElementById('largeImage');
+//
+//      // Unhide image elements
+//      //
+//      largeImage.style.display = 'block';
+//
+//      // Show the captured photo
+//      // The inline CSS rules are used to resize the image
+//      //
+//      largeImage.src = imageURI;
+
+        $scope.images.add(imageURI);
+
+    };
+
+function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
+    };
+
+
+    function capturePhotoEdit() {
+      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+        destinationType: destinationType.DATA_URL });
+    };
+
+
+    function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+    };
+
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    };
+
+
  });

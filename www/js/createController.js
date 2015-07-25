5619eb7d7ea8de251c16711c6925c898447d2515
Cameron Lilly <cameronlilly@maurasoftware.com>
2015-07-25 15:41:29 -0600

    Fixed radio selection border of ugliness. Camera works but needs upgrades.

--- www/js/createController.js
+++ www/js/createController.js
@@ -77,48 +77,87 @@
      $scope.images = [];
 
 
-        var onPhotoDataSuccess = function(imageURI)  {
-            $scope.images.add(imageURI);
-            console.log(imageURI);
-        };
 
-        var onFail = function(message) {
-            alert("FAIL: " + message);
-        };
+  var pictureSource;
+  var destinationType;
+  var imageLocation;
 
 
-    $scope.capturePhoto = function () {
-        console.log("Capture Photo!");
+   // device APIs are available
+  function onDeviceReady() {
+      //alert("Calls when app starts");
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+    }
+      document.addEventListener("deviceready",onDeviceReady,false);
+  // Called when a photo is successfully retrieved
+    function onPhotoDataSuccess(imageData) {
+        //alert("Calls after photo is taken, returning to device");
 
-        // Take picture using device camera and retrieve image as base64-encoded string
-        navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-            quality: 50,
-            destinationType: destinationType.FILE_URI
-        });
+        // Get image handle
+        var smallImage = document.getElementById('smallImage');
 
+        // Unhide image elements
+        smallImage.style.display = 'block';
+
+        // Show the captured photo
+        // The inline CSS rules are used to resize the image
+        smallImage.src = "data:image/jpeg;base64," + imageData;
+    }
+
+  // Called when a photo is successfully retrieved
+    function onPhotoURISuccess(imageURI) {
+      //alert("Calls when we change to add item page");
+
+      // Get image handle
+      //
+      var smallImage = document.getElementById('smallImage');
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      smallImage.src = imageURI;
+
+
+        imageLocation = imageURI;
+    }
+
+  // Capture Photo button will call this function
+  $scope.capturePhoto = function capturePhoto() {
+      //alert("Call when button is pressed");
+
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, $scope.onFail(), { quality: 50,
+        destinationType: destinationType.DATA_URL });
+    }
+
+   // A button will call this function
+    $scope.getPhoto = function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, $scope.onFail(), { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+    }
+
+   // Called if something bad happens.
+    $scope.onFail = function onFail(message) {
+      //alert('Failed because: ' + message);
+    }
+
+    //Cancel the add opperation
+    $scope.cancel = function() {
+        window.history.back();
     };
 
-
-    $scope.capturePhotoEdit = function () {
-        // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-        navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-            quality: 50,
-            allowEdit: true,
-            destinationType: destinationType.FILE_URI
-        });
+    //Complete the save operation
+    $scope.save = function() {
+        $scope.addItem();
+        window.history.back();
     };
 
+     $scope.exportReport = function() {
 
-    $scope.getPhoto = function (source) {
-        // Retrieve image file location from specified source
-        navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-            quality: 50,
-            destinationType: destinationType.FILE_URI,
-            sourceType: source
-        });
-    };
-
-
+     };
 
 
  });

7305c6f2a9ac7414930ae2adea6f96718a8fe28d
Bryce Call <brycecall@sparke.io>
2017-09-06 22:19:22 -0600

    added background to login screen and cleaned up layout

--- mobile2/www/pages/home/home.js
+++ mobile2/www/pages/home/home.js
@@ -9,17 +9,6 @@ app.config(function ($stateProvider) {
 });
 
 // Define the page controller
-app.controller('home', function ($scope, $rootScope, $state, action_manager) {
+app.controller('home', function ($scope, $rootScope) {
   console.log('Welcome Home');
-
-  action_manager.addAction("New Appointment", "watch_later", function () {
-    console.log("NEW APT");
-  });
-  action_manager.addAction("Create Report", "mode_edit", function () {
-    console.log("CREATE");
-    $state.go("reports");
-  });
-  action_manager.addAction("Send a Report", "send", function () {
-    console.log("CREATE");
-  });
 })

140db41be10b273d9759311aac1a9817dcffc19d
Bryce Call <brycecall@sparke.io>
2017-09-09 14:32:41 -0600

    Started on-device PDF generation

--- mobile2/www/pages/reports/reports.js
+++ mobile2/www/pages/reports/reports.js
@@ -16,20 +16,31 @@ app.config(function ($stateProvider) {
       templateUrl: "pages/reports/reports_preview.html",
       controller: "reports_preview"
     })
-    ;    
+    .state('reports_send', {
+      url: "/reports/send",
+      templateUrl: "pages/reports/reports_send.html",
+      controller: "reports_send"
+    });
 });
 
 // Define the page controller
-app.controller('reports', function($scope, $rootScope, camera_manager) {
-    console.log('welcome to reports');
-    $scope.camera_manager = camera_manager;
-    
+app.controller('reports', function ($scope, $rootScope, camera_manager) {
+  console.log('welcome to reports');
+  $scope.camera_manager = camera_manager;
+
 });
 
-app.controller('reports_detail', function($scope, $rootScope) {
-  
+app.controller('reports_detail', function ($scope, $rootScope) {
+
 });
 
-app.controller('reports_preview', function($scope, $rootScope) {
-  
+app.controller('reports_preview', function ($scope, $rootScope) {
+  var doc = new jsPDF;
+  doc.fromHTML(document.querySelector('body').innerHTML, 15, 15);
+  $scope.pdf = doc.output('bloburi');
+  console.log($scope.pdf);
+});
+
+app.controller('reports_send', function ($scope, $rootScope) {
+
 });

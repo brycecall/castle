26c8454918ef84bf0fda23249be687ae773c6f11
Bryce Call <brycecall@sparke.io>
2017-09-12 15:57:46 -0600

    Removing merge issues

--- mobile2/www/pages/login/login.js
+++ mobile2/www/pages/login/login.js
@@ -3,9 +3,9 @@ app.config(function ($stateProvider) {
   $stateProvider
     .state("login", {
       url: "/login",
-    templateUrl: "pages/login/login.html",
-    controller: "login"
-  });
+      templateUrl: "pages/login/login.html",
+      controller: "login"
+    });
 });
 
 app.run(function ($transitions, $rootScope) {
@@ -15,13 +15,6 @@ app.run(function ($transitions, $rootScope) {
       // User isn't authenticated. Redirect to a new Target State
       return trans.router.stateService.target('login');
     }
-<<<<<<< HEAD
-=======
-    /*    if ($rootScope.authenticated !== true && trans.$to().name !== 'camera') {
-          // User isn't authenticated. Redirect to a new Target State
-          return trans.router.stateService.target('camera');
-        }*/
->>>>>>> master
   });
 });
 
@@ -45,20 +38,20 @@ app.controller('login', function ($scope, $rootScope, $state, action_manager, he
     var valid = database.validCredentials($scope.user.username, $scope.user.password);
     valid.then(
       //success - valid user
-      function(promise) {
+      function (promise) {
         console.log(promise.message);
         $rootScope.authenticated = true;
         $state.go("home");
       },
       //fail - invalid user
-      function(promise) {
+      function (promise) {
         console.log(promise.message);
       }
     );
   });
-    
-  action_manager.addAction("Register", "check", function() {
-    database.createUser($scope.new_user.username, $scope.new_user.password, $scope.new_user.email); 
+
+  action_manager.addAction("Register", "check", function () {
+    database.createUser($scope.new_user.username, $scope.new_user.password, $scope.new_user.email);
   });
 
   action_manager.mode = ACTION_MODES.Action;

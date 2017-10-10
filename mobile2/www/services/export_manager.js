5daa4e0397d222519a4ec18a003f8a13c29378f6
Bryce Call <brycecall@sparke.io>
2017-10-10 00:01:02 -0600

    Export file saving is working!  (Now we need to put actual data in it)

--- mobile2/www/services/export_manager.js
+++ mobile2/www/services/export_manager.js
@@ -1,10 +1,10 @@
 app.factory('export_manager', function ($rootScope, $cordovaFileTransfer, $sha) {
   var public = {};
   var private = {};
-  
-  public.export = function(resource, type) {
-    var filename = $sha.hash((new Date()).toString()) + ".castle";
-    
+
+  public.export = function (resource, type) {
+    var filename = $sha.hash((new Date()).toString()).substr(0, 12);
+
     switch (type) {
       case "inspection":
         break;
@@ -15,19 +15,37 @@ app.factory('export_manager', function ($rootScope, $cordovaFileTransfer, $sha) 
       default:
         break;
     }
+
+    var path_to_directory = cordova.file.dataDirectory + "themes";
+    var path_to_castle_file = cordova.file.externalRootDirectory;
+    JJzip.zip(path_to_directory, {
+        target: path_to_castle_file,
+        name: filename
+      },
+      function (result) {
+        console.log(result);
+        if (window['cordova'] !== undefined && result.success) {
+          cordova.plugins.email.open({
+            attachments: path_to_castle_file + filename + ".zip"
+          })
+        }
+      },
+      function (error) {
+        console.log(error);
+      })
   }
-  
-  private.saveInspection = function() {
-    
+
+  private.saveInspection = function () {
+
   };
-  
-  private.saveTemplate = function() {
-    
+
+  private.saveTemplate = function () {
+
   };
-  
-  private.saveTheme = function() {
-    
+
+  private.saveTheme = function () {
+
   };
-  
+
   return public;
 });

f55cd0058f5b8047c8d3d2e94cb2943f08e39fb6
Bryce Call <brycecall@sparke.io>
2017-10-12 01:07:59 -0600

    Export manage cleans up better now, and photos are saved

--- mobile2/www/services/export_manager.js
+++ mobile2/www/services/export_manager.js
@@ -13,68 +13,80 @@ app.factory('export_manager', function ($rootScope, $cordovaFile, $sha, $q, them
     $cordovaFile.createDir(cordova.file.cacheDirectory, private.session_id, true)
       .then(function () {
 
-        switch (type) {
-          case "inspection":
-            if (!resource.insId)
-              promise = private.saveInspection(resource);
-            else
-              promise = private.saveInspection(resource.insId);
-            break;
-          case "template":
-            if (!resource.insId)
-              promise = private.saveTemplate(resource);
-            else
-              promise = private.saveTemplate(resource.insId);
-            break;
-          case "theme":
-            if (!resource.unique)
-              promise = private.saveTheme(resource);
-            else
-              promise = private.saveTheme(resource.unique);
-            break;
-          default:
-            throw type + " is not a valid export type.";
-            break;
-        }
+          switch (type) {
+            case "inspection":
+              if (!resource.insId)
+                promise = private.saveInspection(resource);
+              else
+                promise = private.saveInspection(resource.insId);
+              break;
+            case "template":
+              if (!resource.insId)
+                promise = private.saveTemplate(resource);
+              else
+                promise = private.saveTemplate(resource.insId);
+              break;
+            case "theme":
+              if (!resource.unique)
+                promise = private.saveTheme(resource);
+              else
+                promise = private.saveTheme(resource.unique);
+              break;
+            default:
+              throw type + " is not a valid export type.";
+              break;
+          }
 
-        promise.then(function (result) {
-          JJzip.zip(private.session_path, {
-              target: cordova.file.externalDataDirectory,
-              name: private.session_id
-            },
-            function (result) {
-              $cordovaFile.copyFile(cordova.file.externalDataDirectory, private.session_id + ".zip", cordova.file.externalDataDirectory, private.session_id + ".castle")
-                .then(
-                  function (result) {
-                    console.log(result);
-                    if (window['cordova'] !== undefined && result) {
-                      cordova.plugins.email.open({
-                          attachments: cordova.file.externalDataDirectory + private.session_id + ".castle"
-                        },
-                        function (result) {
-                          $cordovaFile.removeRecursively(cordova.file.cacheDirectory, private.session_id);
-                          $cordovaFile.removeFile(cordova.file.externalDataDirectory, private.session_id + ".zip");
-                          $cordovaFile.removeFile(cordova.file.externalDataDirectory, private.session_id + ".castle");
-
-                          private.session_id = null;
-                          private.session_path = null;
+          promise.then(function (result) {
+              JJzip.zip(private.session_path, {
+                  target: cordova.file.externalDataDirectory,
+                  name: private.session_id
+                },
+                function (result) {
+                  $cordovaFile.copyFile(cordova.file.externalDataDirectory, private.session_id + ".zip", cordova.file.externalDataDirectory, private.session_id + ".castle")
+                    .then(
+                      function (result) {
+                        console.log(result);
+                        if (window['cordova'] !== undefined && result) {
+                          cordova.plugins.email.open({
+                              attachments: cordova.file.externalDataDirectory + private.session_id + ".castle"
+                            },
+                            function (result) {
+                              console.log(result);
+                              private.cleanup();
+                            }
+                          );
                         }
-                      );
-                    }
-                  }
-                );
+                      }
+                    );
+                },
+                function (error) {
+                  conosle.error(error);
+                  private.cleanup();
+                });
             },
             function (error) {
               console.error(error);
-              private.session_id = null;
-              private.session_path = null;
+              private.cleanup();
             });
+        },
+        function (error) {
+          console.error(error);
+          private.cleanup();
         });
-      });
   }
 
   public.import = function (file) {
     // TODO: make this work
+  }
+
+  private.cleanup = function () {
+    $cordovaFile.removeRecursively(cordova.file.cacheDirectory, private.session_id);
+    $cordovaFile.removeFile(cordova.file.externalDataDirectory, private.session_id + ".zip");
+    $cordovaFile.removeFile(cordova.file.externalDataDirectory, private.session_id + ".castle");
+
+    private.session_id = null;
+    private.session_path = null;
   }
 
   private.saveInspection = function (insId) {
@@ -141,11 +153,30 @@ app.factory('export_manager', function ($rootScope, $cordovaFile, $sha, $q, them
   };
 
   private.saveTheme = function (unique) {
+    var defered = $q.defer();
+
     if (!private.session_id) {
       return;
     }
 
-    return $cordovaFile.copyDir(cordova.file.dataDirectory + "themes/", unique, private.session_path + "/", "theme");
+    // Remove the old theme dir (if not cleaned up)
+    $cordovaFile.removeRecursively(private.session_path + "/", "theme").then(function (result) {
+      copyTheme();
+    }, function (error) {
+      copyTheme();
+    });
+    
+    var copyTheme = function() {
+      $cordovaFile.copyDir(cordova.file.dataDirectory + "themes/", unique, private.session_path + "/", "theme").then(
+        function (result) {
+          defered.resolve(result);
+        },
+        function (error) {
+          defered.reject(error);
+        });
+    }
+
+    return defered.promise;
   };
 
   return public;

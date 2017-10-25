25d61c3e7114d3b5e653af9f6e00a71e658f1645
Bryce Call <brycecall@sparke.io>
2017-10-24 22:19:08 -0600

    Updates to the report worker

--- mobile2/www/pages/report/report_worker.js
+++ mobile2/www/pages/report/report_worker.js
@@ -2,33 +2,33 @@ self.onmessage = function (message) {
   var report_buffer = message.data;
   var report = report_buffer.replace("data:application/pdf;base64,", '');
   report = base64Blob(report, "application/pdf");
-  
+
   self.postMessage(report);
 }
 
 // SEE: https://ourcodeworld.com/articles/read/230/how-to-save-a-pdf-from-a-base64-string-on-the-device-with-cordova
-  var base64Blob = function (b64Data, contentType, sliceSize) {
-    contentType = contentType || '';
-    sliceSize = sliceSize || 512;
+var base64Blob = function (b64Data, contentType, sliceSize) {
+  contentType = contentType || '';
+  sliceSize = sliceSize || 512;
 
-    var byteCharacters = atob(b64Data);
-    var byteArrays = [];
+  var byteCharacters = atob(b64Data);
+  var byteArrays = [];
 
-    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-      var slice = byteCharacters.slice(offset, offset + sliceSize);
+  for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    var slice = byteCharacters.slice(offset, offset + sliceSize);
 
-      var byteNumbers = new Array(slice.length);
-      for (var i = 0; i < slice.length; i++) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
-
-      var byteArray = new Uint8Array(byteNumbers);
-
-      byteArrays.push(byteArray);
+    var byteNumbers = new Array(slice.length);
+    for (var i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
     }
 
-    var blob = new Blob(byteArrays, {
-      type: contentType
-    });
-    return blob;
+    var byteArray = new Uint8Array(byteNumbers);
+
+    byteArrays.push(byteArray);
   }
+
+  var blob = new Blob(byteArrays, {
+    type: contentType
+  });
+  return blob;
+}

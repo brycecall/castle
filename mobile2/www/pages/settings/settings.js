fd64c1882f8837b28700cee296da85bc750b8e9c
Bryce Call <brycecall@sparke.io>
2017-11-01 21:27:05 -0600

    Let the DB finish on reinit

--- mobile2/www/pages/settings/settings.js
+++ mobile2/www/pages/settings/settings.js
@@ -8,25 +8,27 @@ app.config(function ($stateProvider) {
     });
 });
 
-app.controller('settings', function ($scope, $rootScope, $cordovaCapture, database, theme_manager) {
+app.controller('settings', function ($scope, $rootScope, $cordovaCapture, $timeout, database, theme_manager) {
 
-  $scope.wipeDatabase = function() {
+  $scope.wipeDatabase = function () {
     var sure = confirm("WARNING!!!\n\nThis will clear all inspection data in the app.\n\nAre you sure you want to do this?");
-    
+
     if (sure) {
       $rootScope.loading = true;
       database.initTables()
-      .then (function() {
-        $scope.reload();
-      }, function (error) {
-        theme_manager.clearThemes();
-        $rootScope.loading = false;
-        alert(error);
-      });
+        .then(function () {
+          $timeout(function () {
+            $scope.reload();
+          }, 8000);
+        }, function (error) {
+          theme_manager.clearThemes();
+          $rootScope.loading = false;
+          alert(error);
+        });
     }
   };
-  
-  $scope.reload = function() {
+
+  $scope.reload = function () {
     window.location.reload();
   }
 });

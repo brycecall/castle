640ebb8b6588b7d2bca87e7bd00d876e303a8c1d
Cameron <ralefire@gmail.com>
2017-12-05 00:17:35 -0700

    Photo mode? Rapid mode?

--- mobile2/www/pages/inspection/inspection.js
+++ mobile2/www/pages/inspection/inspection.js
@@ -21,6 +21,17 @@ app.config(function ($stateProvider) {
         'subsectionIndex': '0',
         'questionIndex': '0'
       }
+    })
+    .state('inspection_photo', {
+      url: "/inspection/photo/{insId}/{sectionIndex}/{subsectionIndex}/{questionIndex}",
+      templateUrl: "pages/inspection/inspection_photo.html",
+      controller: "inspection_photo",
+      params: {
+        'insId': null,
+        'sectionIndex': '0',
+        'subsectionIndex': '0',
+        'questionIndex': '0'
+      }
     });
 });
 
@@ -233,6 +244,10 @@ app.controller('inspection_detail', function ($rootScope, $scope, $, $state, hea
       //});
   });
 
+  header_manager.addAction("Photo Mode", "shutter_camera", function () {
+      $state.go("inspection_photo", {'insId':$scope.insId});
+  }, 'md-raised md-accent');
+    
   $scope.edit = function () {
     $state.go('template_detail', {
       'insId': $scope.insId,
@@ -437,5 +452,73 @@ app.controller('inspection_detail', function ($rootScope, $scope, $, $state, hea
       'title': 'Major Concerns'
         }
     ];
+
+});
+
+app.controller('inspection_photo', function ($rootScope, $scope, $, $state, header_manager, camera_manager, action_manager, inspection_manager, $transition$, shareService) {
+  $scope.insId = $transition$.params().insId;
+  $scope.sectionIndex = $transition$.params().sectionIndex;
+  $scope.subsectionIndex = $transition$.params().subsectionIndex;
+  $scope.questionIndex = $transition$.params().questionIndex;
+  $scope.step = 0;
+  $scope.message = "Choose a Section";
+  header_manager.mode = HEADER_MODES.Action;
+  header_manager.setAction('Back', 'check', function () {
+    $rootScope.loading = true;
+        $rootScope.loading = false;
+        window.history.back();
+  });
+  header_manager.addAction("Wizard Mode", "shutter_camera", function () {
+      $state.go("inspection_detail", {'insId':$scope.insId});
+  }, 'md-raised md-accent');
+    
+  inspection_manager.mode = "inspection";
+  inspection_manager.returnLocation = {
+    'name': $state.current.name,
+    'params': $transition$.params()
+  };
+  $scope.chipList = [];     
+  inspection_manager.getInspection($scope.insId)
+    .then(
+      function (data) {
+        $rootScope.loading = false;
+        $scope.inspection = data.value;
+        $scope.chipList = $scope.inspection.sections;
+      },
+      function (data) {
+        $rootScope.loading = false;
+        console.log("Error... no question exists in the database");
+      }
+    );
+    
+    $scope.stepUp = function(index) {
+        $scope.step++;
+        switch($scope.step) {
+            case 1:
+                $scope.sectionIndex = index;
+                $scope.message = "Choose a Subsection";
+                $scope.chipList = $scope.inspection.sections[index].subsections;
+                break;
+            case 2:
+                $scope.subsectionIndex = index;
+                $scope.message = "Choose a Question";
+                $scope.chipList = $scope.inspection.sections[$scope.sectionIndex].subsections[index].questions;
+                break;
+            case 3:
+                $scope.question = index;
+                $state.go('inspection_detail', {
+                    'insId': $scope.insId,
+                    'sectionIndex': $scope.sectionIndex,
+                    'subsectionIndex': $scope.subsectionIndex,
+                    'questionIndex': $scope.question
+                });
+                break;
+            case 4:
+                break;
+            default:
+        }
+    };
+
+    
 
 });

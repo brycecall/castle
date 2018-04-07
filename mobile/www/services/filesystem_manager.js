6e86abfe4acf9813e536a74f9389dff23987aab7
Bryce Call <brycecall@invenio.xyz>
2018-04-07 21:15:42 +0000

    Added message_manager to init of app

--- mobile/www/services/filesystem_manager.js
+++ mobile/www/services/filesystem_manager.js
@@ -34,6 +34,11 @@ app.factory('filesystem_manager', function ($q, $cordovaFile, $cordovaFileTransf
         function init() {
             var queue = [];
             console.log("INITIALIZING ALL DATA!!!");
+            message_manager.register("assets/idea.jpg", "Welcome to Castle by Invenio!");
+            message_manager.register("assets/feedback.jpg", 'We do software differently!  Use the <md-icon md-svg-icon="help_outline"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="white" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M11,18H13V16H11V18M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,6A4,4 0 0,0 8,10H10A2,2 0 0,1 12,8A2,2 0 0,1 14,10C14,12 11,11.75 11,15H13C13,12.75 16,12.5 16,10A4,4 0 0,0 12,6Z" /></svg></md-icon> to submit feedback and help shape the future.');
+            message_manager.register("assets/gift.jpg", "We've got the basics down, but this is just the beginning. We need your help to make the vision a reality.");
+            message_manager.register("assets/paper.jpg", "Let's get to it, and let us know what you think.");
+
             queue.push(public.resetThemes());
             queue.push(private.deleteInit());
             $q.all(queue).then(defered.resolve, defered.reject);
